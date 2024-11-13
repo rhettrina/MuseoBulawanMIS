8080c7ee863d1a9448a98e5f9938c1bfaa598aaa
k3mosh <cocsmurftzy@gmail.com>
2024-11-13 18:06:56 +0800

    commit ulet

--- Management/Donation/db_conn.php
+++ Management/Donation/db_conn.php
@@ -1,13 +1,13 @@
 <?php
-// db_conn.php
 
-$user = 'root';
-$pass = '';
-$dbName = 'donation';
-$db = new mysqli('localhost', $user, $pass, $dbName);
+    $database_server = "localhost";
+    $database_user = "u376871621_bomb_squad";
+    $database_password = "Fujiwara000!";
+    $database_name = "u376871621_mb_mis";
+    $db = mysqli_connect($database_server, $database_user, $database_password, $database_name);
 
-// Check connection
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-}
+
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
 ?>
