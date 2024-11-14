c668c6e9955b465903afe006335163a112ab5b76
k3mosh <cocsmurftzy@gmail.com>
2024-11-14 10:55:09 +0800

    upd tangina

--- Museo Bulawan Visitor/Donation Form/process_donation.php
+++ Museo Bulawan Visitor/Donation Form/process_donation.php
@@ -1,17 +1,14 @@
 <?php
-// Database configuration
-$servername = "localhost";  // Change this if your database server is not local
-$username = "u376871621_bomb_squad";  // Your MySQL username
-$password = "Fujiwara000!";  // Your MySQL password
-$dbname = "u376871621_mb_mis";  // The name of your database
+ $database_server = "localhost";
+ $database_user = "u376871621_bomb_squad";
+ $database_password = "Fujiwara000!";
+ $database_name = "u376871621_mb_mis";
+ $db = mysqli_connect($database_server, $database_user, $database_password, $database_name);
 
-// Connect to the database
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+ if ($db->connect_error) {
+     die("Connection failed: " . $db->connect_error);
+ }
 
 // Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
