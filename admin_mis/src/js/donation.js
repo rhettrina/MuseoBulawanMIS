6ede3ca54c67107a353d3f93d549308e7f26429d
k3mosh <cocsmurftzy@gmail.com>
2024-11-20 15:07:30 +0800

    test commit

--- admin_mis/src/js/donation.js
+++ admin_mis/src/js/donation.js
@@ -2,3 +2,11 @@ function init(){
     //call the display functions here
 
 }
+
+const dropdownButtons = document.querySelectorAll('.dropdown-menu');
+        dropdownButtons.forEach(button => {
+            button.previousElementSibling.addEventListener('click', () => {
+                const menu = button;
+                menu.classList.toggle('hidden');
+            });
+        });
