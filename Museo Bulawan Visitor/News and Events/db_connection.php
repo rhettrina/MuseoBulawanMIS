fc568e22699c71f0ac7c73116367849347440de6
hachisnail <jeffersontalagtag06@gmail.com>
2024-11-22 03:11:15 +0800

    small things

--- Museo Bulawan Visitor/News and Events/db_connection.php
+++ Museo Bulawan Visitor/News and Events/db_connection.php
@@ -15,7 +15,5 @@ $connection = new mysqli($host, $username, $password, $database);
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
-else{
-    echo'tama';
-}
+
 ?>
