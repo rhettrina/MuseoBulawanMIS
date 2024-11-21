9661498b50495449e483ad213e5c1fd543885ceb
hachisnail <jeffersontalagtag06@gmail.com>
2024-11-22 02:56:57 +0800

    daehthrefmg

--- Museo Bulawan Visitor/News and Events/db_connection.php
+++ Museo Bulawan Visitor/News and Events/db_connection.php
@@ -15,4 +15,7 @@ $connection = new mysqli($host, $username, $password, $database);
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
+else{
+    echo'tama';
+}
 ?>
