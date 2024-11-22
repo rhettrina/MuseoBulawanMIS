0c87e6ac70e6b1ce2308697e74055e4f7558f477
JoroDesu <russelguiwan@gmail.com>
2024-11-22 12:38:30 +0800

    hohotdog360

--- admin_mis/src/php/fetchAppointments.php
+++ admin_mis/src/php/fetchAppointments.php
@@ -13,14 +13,9 @@ include 'db_connect.php';
 $order = isset($_GET['sort']) && $_GET['sort'] === 'date-oldest' ? 'ASC' : 'DESC';
 
 // Fetching data
-$sql = "
-SELECT 
-    preferred_date AS date,
-    CONCAT(first_name, ' ', last_name) AS donor_name,
-    preferred_time AS time,
-    attendees AS number
-FROM form_data
-ORDER BY preferred_date $order, preferred_time $order
+$sql = "SELECT preferred_date AS date, CONCAT(first_name, ' ', last_name) AS donor_name, preferred_time AS time, attendees AS number
+    FROM form_data
+    ORDER BY preferred_date $order, preferred_time $order
 ";
 $result = $conn->query($sql);
 
