fcd0437828d7fa485d23e8ec48301c244ac32c38
k3mosh <cocsmurftzy@gmail.com>
2024-11-23 19:43:24 +0800

    test ule

--- Museo Bulawan Visitor/Lending_Form/processlending.php
+++ Museo Bulawan Visitor/Lending_Form/processlending.php
@@ -141,11 +141,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Insert query for the Artifact table
     $sql_artifact = "INSERT INTO `Artifact`(`artifact_typeID`, `donatorID`, `artifact_description`, `artifact_nameID`, `acquisition`, `additional_info`, `narrative`, `artifact_img`, `documentation`, `related_img`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql_artifact);
+    $stmt->bind_param("sissssssss", $type, $donatorID, $artifactDescription, $artifactTitle, $acquisition, $additionalInfo, $narrative, $art_img_name, $doc_img_name, $rel_img_name);
 
     $type = "Lending";  // Setting a default value for artifact type
 
     // Bind the parameters to the prepared statement
-    $stmt->bind_param("ssssssssss", $type, $donatorID, $artifactDescription, $artifactTitle, $acquisition, $additionalInfo, $narrative, $art_img_name, $doc_img_name, $rel_img_name);
+    $stmt->bind_param("sissssssss", $type, $donatorID, $artifactDescription, $artifactTitle, $acquisition, $additionalInfo, $narrative, $art_img_name, $doc_img_name, $rel_img_name);
 
     // Execute the query
     if ($stmt->execute()) {
