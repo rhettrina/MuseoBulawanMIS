e5e742facec9eaaf5c4f3cf1e25cdd2dd7bc960c
k3mosh <cocsmurftzy@gmail.com>
2024-11-24 01:58:11 +0800

    afafafs

--- admin_mis/src/php/getAppointment.php
+++ admin_mis/src/php/getAppointment.php
@@ -30,25 +30,28 @@ if (!filter_var($appointmentID, FILTER_VALIDATE_INT)) {
 // Prepare the query
 $query = "
     SELECT 
-        a.appointmentID, 
-        v.name , 
-        v.email, 
-        v.phone , 
-        v.address , 
-        v.organization AS visitor_organization, 
-        a.population_countID AS number_of_attendees,  
-        a.preferred_date AS appointment_date, 
-        a.preferred_time AS appointment_time, 
-        a.notes AS appointment_notes, 
-        a.status AS appointment_status, 
-        a.confirmation_date AS appointment_confirmation_date, 
-        a.created_at AS appointment_created_at
-    FROM 
-        appointment AS a
-    JOIN 
-        visitor AS v ON a.visitorID = v.visitorID
-    WHERE 
-        a.appointmentID = ?
+    a.appointmentID AS appointment_id, 
+    v.name AS visitor_name, 
+    v.email AS visitor_email, 
+    v.phone AS visitor_phone, 
+    v.address AS visitor_address, 
+    v.organization AS visitor_organization, 
+    a.population_countID AS number_of_attendees,  
+    a.preferred_date AS preferred_date, 
+    a.preferred_time AS preferred_time, 
+    a.purpose AS appointment_purpose, /* Added purpose field */
+    a.notes AS appointment_notes, 
+    a.status AS appointment_status, 
+    a.confirmation_date AS confirmation_date, 
+    a.created_at AS created_at
+FROM 
+    appointment AS a
+JOIN 
+    visitor AS v ON a.visitorID = v.visitorID
+WHERE 
+    a.appointmentID = ?
+
+
 ";
 
 $stmt = $connection->prepare($query);
